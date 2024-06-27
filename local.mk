356cbb47a1e346f694a0758cb66abf533d723c9e
MORIMOTO Jun <morimoto@gmail.com>
2024-06-27 15:20:42 +0900

    サンプルファイルを切り分け

--- local.mk
+++
@@ -1,8 +0,0 @@
-# github.com/mrmt/anno2024
-# 各自のローカル定義を書くところ
-
-URL_TAMA = 'https://www.google.com/maps/d/viewer?hl=ja&mid=1ZiG9f7Zy_yeFmmcVENK0tWt1YGghdtk&ll=35.743870311984374%2C139.4759485&z=12'
-URL_WEST = 'https://www.google.com/maps/d/viewer?hl=ja&mid=1vta483ns58fvyi8oWecYCy0nkGcEmqY&ll=35.66905955832993%2C139.66700335&z=11'
-
-map:
-	open $(URL_TAMA) $(URL_WEST)
